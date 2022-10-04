d2855d211397ce7831f941ebe6cab0e622968be4
Rad Juan <radiantcjuan@gmail.com>
2022-10-04 23:34:38 +0800

    addded resource for the API requests, fixed the form action

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'comments' => json_decode($this->comments),
+            'name' => $this->name,
+            'img' => Storage::url('users/'.$this->id.'.jpg')
+        ];
+    }
+}
